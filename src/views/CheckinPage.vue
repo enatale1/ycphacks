a4583bd676dbd96c3476991c0cee8cfc4f3aa8f6
Evan Natale <enatale1@ycp.edu>
2026-02-10 00:00:07 -0500

    Added QR code

--- src/views/CheckinPage.vue
+++ src/views/CheckinPage.vue
@@ -6,15 +6,44 @@
           {{ 'Show this QR code to staff' }}
         </h1>
         <hr class="header-line" />
+        <div class="mt-4" v-if="qrCode">
+          <img
+              :src="qrCode"
+              alt="Check-in QR Code"
+              class="qr-image"
+          />
+        </div>
       </div>
     </header>
   </div>
 </template>
 
 <script setup>
-import { mapGetters } from "vuex";
-import axios from 'axios';
+import { ref, onMounted } from 'vue'
+import { useStore } from 'vuex'
+import axios from 'axios'
 
+const store = useStore()
+const qrCode = ref('')
+
+onMounted(async () => {
+  try {
+    const user = store.getters.getUser
+
+    if (!user) {
+      console.error('User not logged in')
+      return
+    }
+
+    const userId = user.id
+    const baseUrl = store.state.apiBaseUrl
+
+    const response = await axios.get(`${baseUrl}/user/${userId}/qrcode`)
+    qrCode.value = response.data.qr
+  } catch (err) {
+    console.error('Failed to load QR code:', err)
+  }
+})
 </script>
 
 <style scoped>
